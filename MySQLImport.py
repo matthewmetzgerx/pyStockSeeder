8bb4b7673fb1bf5f40af160a3c8f901825bebfd5
Matthew Metzger <mmetzger@my.bellevue.edu>
2019-06-18 21:57:40 -0400

    Updates to support the import of sp500, nasdaq, vix and dow historical into mysql.

--- MySQLImport.py
+++ MySQLImport.py
@@ -2,7 +2,7 @@ import json
 import pymysql
 import pandas as pd
 import sqlalchemy
-
+import sys
 
 try:
     with open('config.json') as config:
@@ -20,31 +20,32 @@ connection = pymysql.connect(host=cfg["MySQL"]["host"],
                              cursorclass=pymysql.cursors.DictCursor)
 
 
-def SQLInsertHistory(file):
-	# creates table and inserts complete stock history file into the table
-	df = pd.read_csv(file)
-	cols = ["Open", "High", "Low", "Close", "Adjusted_close", "Volume"]
-	df[cols].apply(pd.to_numeric, errors='coerce')
-	df["Date"] = pd.to_datetime(df['Date'])
+def SQLInsertHistory(file, table):
+    # creates table and inserts complete stock history file into the table
+    df = pd.read_csv(file)
+    adjcls = "Adjusted_close" if "Adjusted_close" in df.columns else "Adj Close"
+    cols = ["Open", "High", "Low", "Close", adjcls, "Volume"]
+    df[cols].apply(pd.to_numeric, errors='coerce')
+    df["Date"] = pd.to_datetime(df['Date'])
 
-	database_username = cfg["MySQL"]["user"]
-	database_password = cfg["MySQL"]["password"]
-	database_ip = cfg["MySQL"]["host"]
-	database_name = cfg["MySQL"]["db"]
+    database_username = cfg["MySQL"]["user"]
+    database_password = cfg["MySQL"]["password"]
+    database_ip = cfg["MySQL"]["host"]
+    database_name = cfg["MySQL"]["db"]
 
-	database_connection = sqlalchemy.create_engine('mysql+mysqlconnector://{0}:{1}@{2}/{3}'.
-												   format(database_username, database_password,
-														  database_ip, database_name,
-														  auth_plugin='mysql_native_password'),
-														  pool_pre_ping=True, pool_recycle=1800)
-	batch = 20000
-	loops = (len(df)//batch) + 1
-	for it in range(0,loops):
-		df[(it*batch):((it+1)*batch)].to_sql(
-			con=database_connection, name=cfg["DBTables"]["history"], if_exists='append'
-		)
-		print("Batch " + str(it + 1) + " of " + str(loops) + " complete.")
-		
+    database_connection = sqlalchemy.create_engine('mysql+mysqlconnector://{0}:{1}@{2}/{3}'.
+                                                   format(database_username, database_password,
+                                                          database_ip, database_name,
+                                                          auth_plugin='mysql_native_password'),
+                                                   pool_pre_ping=True, pool_recycle=1800)
+    batch = 20000
+    loops = (len(df) // batch) + 1
+    for it in range(0, loops):
+        df[(it * batch):((it + 1) * batch)].to_sql(
+            con=database_connection, name=table, if_exists='append'
+        )
+        print("Batch " + str(it + 1) + " of " + str(loops) + " complete.")
+
 
 def SQLInsertStockfile(file):
     # creates table and inserts the stock profile info into a table
@@ -67,8 +68,21 @@ def main():
     # in which stock history updates can be made and added to the history
     # additionally, another condition will be made that permits adding more
     # stock profile symbols
-    SQLInsertStockfile(cfg["writeseeder"])
-    SQLInsertHistory(cfg["allHistory"])
+
+    try:
+        arg = sys.argv[1]
+    except:
+        arg = "all"
+
+    if (arg == "all" or arg == "stocklist"):
+        SQLInsertStockfile(cfg["writeseeder"])
+
+    if (arg == "all" or arg == "stockhistory"):
+        SQLInsertHistory(cfg["allHistory"], cfg["DBTables"]["history"])
+
+    if (arg == "all" or arg == "indexes"):
+        for ind in cfg["indexFiles"]:
+            SQLInsertHistory(ind["writeTo"], cfg["DBTables"][ind["relator"]])
 
 
 main()
